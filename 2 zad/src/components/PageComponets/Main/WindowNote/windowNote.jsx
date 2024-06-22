eb2fd76fc4803b8d24c0a4a14214097cf24bbee6
Ярослав <109422607+9archik@users.noreply.github.com>
2024-06-23 02:24:15 +0300

    добавил валидацию в поле добавления задачи

--- 2 zad/src/components/PageComponets/Main/WindowNote/windowNote.jsx
+++ 2 zad/src/components/PageComponets/Main/WindowNote/windowNote.jsx
@@ -1,6 +1,7 @@
 import Input from "../../../UI/Input/Input";
 import styles from "./style.module.css";
 import Button from "../../../UI/Button/Button";
+import { useState } from "react";
 const WindowNote = ({
   valueText,
   cancelClick,
@@ -10,23 +11,33 @@ const WindowNote = ({
   textHeader,
   submitBtnText,
 }) => {
+  const [textError, setTextError] = useState(null);
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault();
+        if (valueText.length === 0) {
+          setTextError("Введите задачу");
+          return;
+        }
+        setTextError(null);
         submitClick();
       }}
       className={`${styles.container}`}
     >
       <div className={styles.header}>{textHeader}</div>
-      <Input
-        className={styles.input}
-        onChange={(event) => {
-          onChangeText(event.target.value);
-        }}
-        value={valueText ? valueText : ""}
-        placeholder={"Input your note..."}
-      />
+
+      <div className={styles.inputBlock}>
+        {textError && <div className={styles.error}>{textError}</div>}
+        <Input
+          className={styles.input}
+          onChange={(event) => {
+            onChangeText(event.target.value);
+          }}
+          value={valueText ? valueText : ""}
+          placeholder={"Input your note..."}
+        />
+      </div>
 
       <div className={styles.buttons}>
         <Button onClick={cancelClick}>Cancel</Button>
